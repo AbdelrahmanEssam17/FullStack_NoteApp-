4058656d5b42d6a4dcbd8d70729f02e54728d603
”abdelrahman <eabdo6990@gmail.com>
2026-07-10 22:18:31 +0300

    make api for addtowishlist

--- BackEnd/src/modules/wishlist/wishlist.controller.js
+++ BackEnd/src/modules/wishlist/wishlist.controller.js
@@ -1,6 +1,31 @@
 import pool from "../../DB/db.connection.js";
 
-export const addTowishlist = async (req, res, next) => {};
+export const addToWishlist = async (req, res) => {
+  try {
+    console.log(req.user);
+    const { note_id } = req.body;
+    const { id: user_id } = req.user;
+
+    console.log(user_id);
+    const result = await pool.query(
+      `
+      INSERT INTO wishlist (user_id, note_id)
+      VALUES ($1, $2)
+      RETURNING *;
+      `,
+      [user_id, note_id],
+    );
+
+    return res.status(201).json({
+      message: "Note added to wishlist successfully",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 export const getAllWishlist = async (req, res, next) => {};
 export const removefromwishlist = async (req, res, next) => {};
 
