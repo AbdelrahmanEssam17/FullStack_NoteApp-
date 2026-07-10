402c5576909e16f55875dc6d343a9f451435a5f9
”abdelrahman <eabdo6990@gmail.com>
2026-07-10 22:30:38 +0300

    adding some package for security

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,6 +5,9 @@ import authroute from "./src/modules/auth/auth.route.js";
 import userroute from "./src/modules/user/user.route.js";
 import noteroute from "./src/modules/note/note.route.js";
 import wishlistroute from "./src/modules/wishlist/wishlist.route.js";
+import cors from "cors";
+import limiter from "express-rate-limit";
+import helmet from "helmet";
 dotenv.config();
 pool
   .connect()
@@ -13,14 +16,20 @@ pool
     client.release();
   })
   .catch((err) => console.log(err));
-
+const limter = limiter({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(limter);
+app.use(cors());
 app.use("/user", userroute);
 app.use("/auth", authroute);
 app.use("/note", noteroute);
 app.use("/wishlist", wishlistroute);
 const port = process.env.PORT;
-app.listen(process.env.PORT, "127.0.0.1", () => {
+app.listen(process.env.PORT, () => {
   console.log(`"server is running in "${port}`);
 });
