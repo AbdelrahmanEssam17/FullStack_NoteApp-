9a83b9c35f4d4fb8b9162b869434609ed3776931
”abdelrahman <eabdo6990@gmail.com>
2026-07-25 17:31:51 +0300

    make the note fetch

--- FrontEnd/NoteApp/src/apis/note.jsx
+++ FrontEnd/NoteApp/src/apis/note.jsx
@@ -1,1 +1,48 @@
 import axios from "axios";
+
+const addnote = async () => {
+  try {
+    const response = await axios.post("http://localhost:3002/note/addnote", {
+      description: "hello",
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.response?.data);
+  }
+};
+
+const deleteall = async () => {
+  try {
+    const response = await axios.post("http://localhost:3002/note/deleteall");
+
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.response?.data);
+  }
+};
+
+const update = async () => {
+  try {
+    const response = await axios.post("http://localhost:3002/note/update", {
+      note_id: "1",
+      description: "updated note",
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.response?.data);
+  }
+};
+
+const deletebyid = async () => {
+  try {
+    const response = await axios.post("http://localhost:3002/note/delete", {
+      note_id: "1",
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.response?.data);
+  }
+};
